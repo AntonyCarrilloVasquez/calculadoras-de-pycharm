4edefc2b2e6e25e39dbdae97ef6242b1ba2dc668
AntonyCarrilloVasquez <56943321+antonycarrillovasquez@unprg.edu.pe>
2019-11-03 18:56:02 -0500

    se agrego el 1er ejercicio

---
+++ ejercicio 04.py
@@ -0,0 +1,292 @@
+#CALCULADORA NRO 01
+#FORMULA DE LA VELOCIDAD FINAL DE CAIDA LIBRE
+
+#VARIABLES
+velocidad_inicial,gravedad,tiempo,velocidad_final=0.0,0.0,0.0,0.0
+#CALCULAMOS
+velocidad_inicial=30
+gravedad=10
+tiempo=15
+velocidad_final=velocidad_inicial+(gravedad*tiempo)
+#MOSTRAR DATOS
+print("velocidad inicial=",velocidad_inicial)
+print("gravedad=",gravedad)
+print("tiempo=",tiempo)
+print("velocidad final=",velocidad_final)
+
+#CALCULADORA NRO 02
+#FORMULA DEL AREA DE UN TRIANGULO
+
+#VARIABLES
+base,altura,area_de_triangulo=0.0,0.0,0.0
+#CALCULAMOS
+base=31
+altura=13
+area_de_triangulo=(base*altura)/2
+#MOSTRAR DATOS
+print("base=",base)
+print("altura=",altura)
+print("area de triangulo=",area_de_triangulo)
+
+
+#CALCULADORA NRO 03
+#FORMULA DEL AREA DE UN CIRUCULO
+
+#VARIABLES
+pi,radio,area_de_circulo=0.0,0.0,0.0
+#CALCULAMOS
+pi=3.1416
+radio=4
+area_de_circulo=(pi*radio)*2
+#MOSTRAR DATOS
+print("pi=",pi)
+print("radio=",radio)
+print("area de circulo=",area_de_circulo)
+
+#CALCULADORA NRO 04
+#FORMULA DE LA DISTANCIA(FISICA)
+
+#VARIABLES
+velocidad,tiempo,distancia=0.0,0.0,0.0
+#CALCULAMOS
+velocidad=82
+tiempo=12
+distancia=velocidad*tiempo
+#MOSTRAR DATOS
+print("velocidad=",velocidad)
+print("tiempo=",tiempo)
+print("distancia=",distancia)
+
+#CALCULADORA NRO 05
+#FORMULA DEL VOLUMEN DE UN PRISMA RECTO
+
+#VARIABLES
+longitud,ancho,altura,volumen_de_prisma_recto=0.0,0.0,0.0,0.0
+#CALCULAMOS
+longitud=45
+ancho=3
+altura=7
+volumen_de_prisma_recto=longitud*ancho*altura
+#MOSTRSR DATOS
+print("longitud=",longitud)
+print("ancho=",ancho)
+print("altura=",altura)
+print("volumen de prisma recto=",volumen_de_prisma_recto)
+
+#CALCULADORA NRO 06
+#FOMULA DE LA FUERZA
+
+#VARIABLES
+masa,aceleracion,fuerza=0.0,0.0,0.0
+#CALCULAMOS
+masa=15
+aceleracion=54
+fuerza=masa*aceleracion
+#MOSTRAR DATOS
+print("masa=",masa)
+print("aceleracion=",aceleracion)
+print("fuera=",fuerza)
+
+#CALCULADORA NRO 07
+#FORMULA DEL COSTO TOTAL
+
+#VARIABLES
+costo_fijo,costo_variable,costo_total=0.0,0.0,0.0
+#CALCULAMOS
+costo_fijo=12
+costo_variable=52
+costo_total=costo_fijo+costo_variable
+#MOSTRAR DATOS
+print("costo fijo=",costo_fijo)
+print("costo variable=",costo_variable)
+print("costo total=",costo_total)
+
+#CALCULADORA NRO 08
+#FORMULA DEL CSOTO FIJO MEDIO
+
+#VARIABLES
+costo_fijo,produccion,costo_fijo_medio=0.0,0.0,0.0
+#CALCULAMOS
+costo_fijo=970
+produccion=36
+costo_fijo_medio=costo_fijo/produccion
+#MOSTRAR DATOS
+print("costo fijo=",costo_fijo)
+print("produccion=",produccion)
+print("costo fijo medio=",costo_fijo_medio)
+
+#CALCULADORA NRO 09
+#FORMULA DE LA POTENCIA
+
+#VARIABLES
+trabajo,tiempo,potencia=0.0,0.0,0.0
+#CALCULAMOS
+trabajo=56
+tiempo=2
+potencia=trabajo/tiempo
+#MOSTRAR DATOS
+print("trabajo=",trabajo)
+print("tiempo=",tiempo)
+print("potencia=",potencia)
+
+#CALCULADORA NRO 10
+#FORMULA DE LA DENSIDAD
+
+#VARIABLES
+masa,volumen,densidad=0.0,0.0,0.0
+#CALCULAMOS
+masa=5.6
+volumen=2.1
+densidad=masa/volumen
+#MOSTRAR DATOS
+print("masa=",masa)
+print("volumen=",volumen)
+print("densidad=",densidad)
+
+#CALCULADORA NRO 11
+#FORMULA DEL PESO
+
+#VARIABLES
+masa,gravedad,peso=0.0,0.0,0.0
+#CALCULAMOS
+masa=5.21
+gravedad=9.8
+peso=masa*gravedad
+#MOSTRAR DATOS
+print("masa=",masa)
+print("gravedad=",gravedad)
+print("peso=",peso)
+
+
+#CALCULADORA NRO 12
+#FORMULA DE LA CAPACITANCIA
+
+#VARIABLES
+carga_electronica_almacenada,diferencia_potencial_aplicada,capacitancia=0.0,0.0,0.0
+#CALCULAMOS
+carga_electronica_almacenada=45.3
+diferencia_potencial_aplicada=9.13
+capacitancia=carga_electronica_almacenada/diferencia_potencial_aplicada
+#MOSTRAR DATOS
+print("carga electronica almacenada=",carga_electronica_almacenada)
+print("diferencia potencial aplicada=",diferencia_potencial_aplicada)
+print("capacitancia=",capacitancia)
+
+#CALCULADORA NRO 13
+#FORMULA DEL COSTO MEDIO
+
+#VARIABLES
+costo_total,produccion,costo_medio=0.0,0.0,0.0
+#CALCULAMOS
+costo_total=12.4
+produccion=4
+costo_medio=costo_total/produccion
+#MOSTRAR DATOS
+print("costo total=",costo_total)
+print("produccion=",produccion)
+print("costo medio=",costo_medio)
+
+
+#CALCULADORA NRO 14
+#FORMULA DEL POTENCIAL ELECTRICO
+
+#VARIABLES
+constante_electrica,carga,distancia,potencial_electronico=0.0,0.0,0.0
+#CALCULAMOS
+constante_electrica=31.5
+carga=9.1
+distancia=12.4
+potencial_electronico=(constante_electrica*carga)/distancia
+#MOSTRAR DATOS
+print("constante electrica=",constante_electrica)
+print("carga=",carga)
+print("distancia=",distancia)
+print("potencial electronico=",potencial_electronico)
+
+#CALCULADORA NRO 15
+#FORMULA DEL AREA DEL RECTANGULO
+
+#VARIABLES
+ancho,longitud,area_de_rectangulo=0.0,0.0,0.0
+#CALCULAMOS
+ancho=12.8
+longitud=5.5
+area_de_rectangulo=ancho+longitud
+#MOSTRAR DATOS
+print("ancho=",ancho)
+print("longitud=",longitud)
+print("area de rectangulo=",area_de_rectangulo)
+
+#CALCULADORA NRO 16
+#FORMULA DE LA VELOCIDAD ANGULAR
+
+#VARIABLES
+desplazamiento_angular,tiempo_recorrido,velocidad_angular=0.0,0.0,0.0
+#CALCULAMOS
+desplazamiento_angular=37
+tiempo_recorrido=5.2
+velocidad_angular=desplazamiento_angular/tiempo_recorrido
+#MOSTRAR DATOS
+print("desplazamiento angular=",desplazamiento_angular)
+print("tiempo recorrido=",tiempo_recorrido)
+print("velocidad angular=",velocidad_angular)
+
+#CALCULADORA NRO 17
+#FORMULA DE LA VELOCIDAD TANGENCIAL
+
+#VARIABLES
+longitud_arco,unidad_tiempo,velocidad_tangencial=0.0,0.0,0.0
+#CALCULAMOS
+longitud_arco=97
+unidad_tiempo=4.2
+velocidad_tangencial=longitud_arco/unidad_tiempo
+#MOSTRAR DATOS
+print("longitud arco=",longitud_arco)
+print("unidad tiempo=",unidad_tiempo)
+print("velocidad tangencial=",velocidad_tangencial)
+
+#CALCULADORA NRO 18
+#FORMULA DEL TIEMPO DE ENCUENTRO
+
+#VARIABLES
+distancia_total,velocidad_uno,velocidad_dos,tiempo_encuentro=0.0,0.0,0.0,0.0
+#CALCULAMOS
+distancia_total=89.60
+velocidad_uno=12
+velocidad_dos=30
+tiempo_encuentro=distancia_total/(velocidad_uno+velocidad_dos)
+#MOSTRAR DATOS
+print("distancia total=",distancia_total)
+print("velocidad uno=",velocidad_uno)
+print("velocidad dos=",velocidad_dos)
+print("tiempo encuentro=",tiempo_encuentro)
+
+#CALCULADORA NRO 19
+#FORMMULA DEL MRUV
+
+#VARIABLES
+velocidad_inicial,aceleracion,tiempo,velocidad_final=0.0,0.0,0.0,0.0
+#CALCULAMOS
+velocidad_inicial=12
+aceleracion=5
+tiempo=7
+velocidad_final=velocidad_inicial+(aceleracion*tiempo)
+#MOSTRAR DATOS
+print("velocidad inicial=",velocidad_inicial)
+print("aceleracion=",aceleracion)
+print("tiempo=",tiempo)
+print("velocidad final=",velocidad_final)
+
+#CALCULADORA NRO 20
+#FORMULA DE LA PRESION
+
+#VARIABLES
+fuerza,area,presion=0.0,0.0,0.0
+#CALCULAMOS
+fuerza=21.5
+area=6.2
+presion=fuerza/area
+#MOSTRAR DATOS
+print("fuerza=",fuerza)
+print("area=",area)
+print("presion=",presion)
